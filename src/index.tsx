3f53aebaf138888df7fa1aa67d6ca509303f4626
mgbanh97 <mgbanh97@gmail.com>
2020-05-12 08:50:33 +0700

    Initialized display video dialog and assignment dialog

--- src/index.tsx
+++ src/index.tsx
@@ -6,31 +6,29 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.js';
 import configStore from './redux/store';
+import './index.css';
 
 const jss = create({
   ...jssPreset(),
-  insertionPoint: 'jss-insertion-point',
+  insertionPoint: 'jss-insertion-point'
 });
 
 const store = configStore();
 ReactDOM.render(
-    <Provider store={store.store as any}>
-        <PersistGate
-            persistor={store.persistor}
-            loading={makeLoading()}
-        >
-        <StylesProvider jss={jss}>
-            <App />
-        </StylesProvider>
-        </PersistGate>
-    </Provider>,
-    document.getElementById('root') 
-  );
+  <Provider store={store.store as any}>
+    <PersistGate persistor={store.persistor} loading={makeLoading()}>
+      <StylesProvider jss={jss}>
+        <App />
+      </StylesProvider>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 
 function makeLoading() {
   return (
-    <div className="main-loading">
-      <div className="lds-dual-ring"></div>
+    <div className='main-loading'>
+      <div className='lds-dual-ring'></div>
     </div>
   );
 }
